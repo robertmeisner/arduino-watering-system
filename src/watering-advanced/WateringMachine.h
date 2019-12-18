50cad272247a13b1efe06c0ff19fc9c456052cc1
Robert Meisner <robert@catchit.pl>
2019-12-18 08:37:51 +0100

    Watering machine base code

--- src/watering-advanced/WateringMachine.h
+++ src/watering-advanced/WateringMachine.h
@@ -1,6 +1,34 @@
 
+#ifndef WateringMachine_h
+#include "state-pattern/WateringMachineStateBase.h"
+#include "Light.h"
+#include "MoistureSensor.h"
+#include "SimplePump.h"
 
+#define WateringMachine_h
 
+class WateringMachine
+{
+private:
+    WateringMachineStateBase state;
+    int lastWateringChrono;
+    int lastLightingChrono;
+    int lightingInterval;
+    int lightingDuration;
+    int lastMoistureChrono;
+    int avgMoisture;
+    Light* light;
+    MoistureSensor* moisture[2];
+    SimplePump* pump;
+    WateringMachine(int lightPin,int moisturePin1,int moisturePin2)
+    {
+        this->light = new Light(lightPin);
+        this->moisture[0]= new MoistureSensor(moisturePin1);
+        this->moisture[1]= new MoistureSensor(moisturePin1);
+        this-<pump=new SimplePump
+    }
+};
+#endif
 //Humidity sensor #1
 //Humidity Sensor #2
 //Pumps switch
---
+++ src/watering-advanced/state-pattern/WateringMachineStateBase.h
@@ -0,0 +1,20 @@
+class WateringMachineStateBase
+{
+public:
+    // Class destructor for WateringMachineStateBase (Declared as virtual)
+    ~WateringMachineStateBase() {}
+    WateringMachineStateBase handleWatering(){
+
+    }
+    WateringMachineStateBase handleLighting(){
+
+    }
+    WateringMachineStateBase handleMoistureReading(){
+
+    }
+
+
+
+protected:
+private:
+};
